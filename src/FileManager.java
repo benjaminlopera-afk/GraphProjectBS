f8b2aec9a9cf058401a7a786d956864642bbadaa
benjaminlopera-afk <benjaminlopera@correounivalle.edu.co>
2026-05-26 14:36:44 -0500

    Add the file manager to read files to done the 4 point of the project

---
+++ src/FileManager.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileManager {
+
+    public static void loadGraphFromFile(
+            String path,
+            Graph graph
+    ) {
+
+        try {
+
+            File file = new File(path);
+
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+
+                String line = scanner.nextLine();
+
+                String[] parts = line.split(",");
+
+                int source = Integer.parseInt(parts[0]);
+                int target = Integer.parseInt(parts[1]);
+                int weight = Integer.parseInt(parts[2]);
+
+                graph.addEdge(source, target, weight);
+            }
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+
+            System.out.println("File not found.");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
 
         Graph graph = new Graph();
 
-        graph.addEdge(0, 1, 5);
-        graph.addEdge(0, 2, 3);
-        graph.addEdge(1, 2, 1);
+        FileManager.loadGraphFromFile("graph.txt", graph);
+
+//        graph.addEdge(0, 1, 5);
+//        graph.addEdge(0, 2, 3);
+//        graph.addEdge(1, 2, 1);
 
         graph.printGraph();
     }
